1c299dfeeb60c67380899b39999d91c88ad4da04
wykeosk <oskar.wyke@gmail.com>
2012-12-29 15:07:23 +0100

    First commit

---
+++ web/js/model.js
@@ -0,0 +1,38 @@
+// Class to represent a row in the seat reservations grid
+function SeatReservation(name, initialMeal) {
+    var self = this;
+    self.name = name;
+    self.meal = ko.observable(initialMeal);
+
+    self.formattedPrice = ko.computed(function() {
+        var price = self.meal().price;
+        return price ? "$" + price.toFixed(2) : "None";        
+    });    
+}
+
+
+function MatchSetup() {
+	var self = this;
+	self.homeTeam = ko.observable("");
+	self.awayTeam = ko.observable("");
+	self.homePlayer = ko.observable("");
+	self.awayPlayer = ko.observable("");
+
+}
+
+
+function Match() {
+	var self = this;
+	self.setup = ko.observable();
+	self.homeGoals = ko.observable();
+	self.awayGoals = ko.observable();
+	self.result = ko.computed(function() {
+		return self.homeGoals() + " - " + self.awayGoals();
+	});
+}
+
+function MatchesWrapper() {
+	var self = this;
+	self.homeTeam = ko.observableArray([]);
+
+}
